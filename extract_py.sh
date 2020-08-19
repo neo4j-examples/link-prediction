9d3eb169efdff2fc39e9072db9d926bc35daab2d
Mark Needham <m.h.needham@gmail.com>
2020-08-19 17:51:45 +0100

    update notebooks

---
+++ extract_py.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+jupytext --to py --output "py/03_Train_Test_Split.py" "notebooks/03_Train_Test_Split.ipynb"
+jupytext --to py --output "py/04_Model_Feature_Engineering.py" "notebooks/04_Model_Feature_Engineering.ipynb"
+jupytext --to py --output "py/05_Train_Evaluate_Model.py" "notebooks/05_Train_Evaluate_Model.ipynb"
