0e2cfac8c7f5bc9834e85f9c3996f1cd5fec65df
Mark Needham <m.h.needham@gmail.com>
2020-08-19 17:53:13 +0100

    add  co author graph

--- extract_py.sh
+++ extract_py.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+jupytext --to py --output "py/02_Co-Author_Graph.py" "notebooks/02_Co-Author_Graph.ipynb"
 jupytext --to py --output "py/03_Train_Test_Split.py" "notebooks/03_Train_Test_Split.ipynb"
 jupytext --to py --output "py/04_Model_Feature_Engineering.py" "notebooks/04_Model_Feature_Engineering.ipynb"
 jupytext --to py --output "py/05_Train_Evaluate_Model.py" "notebooks/05_Train_Evaluate_Model.ipynb"
